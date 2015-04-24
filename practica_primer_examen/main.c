1ef4207c9dba8b640352ca6b9d166af581236983
davilamr <davilamr.80@gmail.com>
2015-04-24 20:23:13 -0300

    pdf

--- practica_primer_examen/main.c
+++ practica_primer_examen/main.c
@@ -4,7 +4,7 @@
  *  Created on: 24/4/2015
  *      Author: root
  */
-#define PAUSE()  do { printf("\n\n-PAUSE- file: %s line:%d\n", __FILE__, __LINE__); getchar(); } while (0)
+#define PAUSE()  do { printf("\n\n--PAUSE- file: %s line:%d\n", __FILE__, __LINE__); getchar(); } while (0)
 
 #include <stdio.h>
 #include <stdlib.h>
